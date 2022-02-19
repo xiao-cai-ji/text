e915027d9754697f262d53d41f174ba2b02ea1ed
xiao-cai-ji <2399616092@qq.com>
2022-02-19 19:49:58 +0800

    第二次

--- src/test/text.java
+++ src/test/text.java
@@ -3,6 +3,11 @@ package test;
 public class text {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123"); System.out.println("123"); System.out.println("123");
+
+
     }
 
 }
